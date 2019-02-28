8af3225d807d7a21be4539e869cbea719661f00c
mischb <mischarberlin@gmail.com>
2019-02-28 11:18:24 -0500

    completed swipeable deck with react-native

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Deck } from './src';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+import DATA from './dummyData';
+export default class App extends React.Component {
+  renderCard(item) {
+    return (
+      <Card key={item.id} image={{ uri: item.uri }}>
+        <Text style={{ marginBottom: 10 }}>{item.text}</Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#04A9F4"
+          title="View Now!"
+        />
+      </Card>
+    );
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card title="No More!">
+        <Text style={{ marginBottom: 10 }}>Done!</Text>
+        <Button backgroundColor="#04A9F4" title="Get More" />
+      </Card>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
